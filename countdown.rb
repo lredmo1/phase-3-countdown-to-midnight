b289acaf580068787299005c6a93826715a5a485
Lisa <redlisa125@gmail.com>
2021-11-01 07:52:33 -1000

    commit tree e3c9fdac2141c9ca6ec962512a4eb108e2d3901f

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+    i = integer
+    sleep(1)
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        
+    i -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+puts countdown(10)
